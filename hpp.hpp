2c8f67d47efd6e1654ee7e1a7c4adc47e007ad75
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-18 09:34:52 +0400

    + skelex
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ hpp.hpp
@@ -0,0 +1,29 @@
+#ifndef _H_HUI
+#define _H_HUI
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string tag,val;
+	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string tagval(); string tagstr(); string pad(int);
+	virtual string dump(int=0);
+};
+
+extern map<string,Sym*> env;
+extern void env_init();
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_HUI
